d0814c3761309b2eed1d45f344c557c47dd01892
Dovid Rosoff <Dovid.rosoff@gmail.com>
2020-10-19 20:17:26 -0400

    Added Auto
    Added auto class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Auto", group="LinearOpMode")
+
+public class Auto extends LinearOpMode{
+    public void runOpMode() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
 public class TeleOp extends LinearOpMode {
     public void runOpMode(){
         
