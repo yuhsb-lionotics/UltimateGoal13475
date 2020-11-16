bc59ef68b9167d39dba6ebe941c0b796c4257b41
Dovid Rosoff <Dovid.rosoff@gmail.com>
2020-11-16 17:00:28 -0500

    Update driveTrain.java
    added a few different methods for drive train. methods will be complteded at next meeting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrain.java
@@ -3,7 +3,38 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 //test
-public class driveTrain {
+public class driveTrain extends LinearOpMode {
+    protected DcMotor fr,fl,br,bl;
+    private ElapsedTime time = new ElapsedTime();
+
+    private static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    private static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    private static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    private static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    @Override
+    public void runOpMode(){
+
+    }
+
+    public void setup(){
+
+    }
+
+    public void driveForward(){
+
+    }
+
+    public void turnLeft(){
+
+    }
+
+    public void turnRight(){
+
+    }
 }
 
