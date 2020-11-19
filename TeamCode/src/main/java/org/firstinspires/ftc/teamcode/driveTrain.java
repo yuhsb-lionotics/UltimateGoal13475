4eefd0e279f0ff79ce479afedcab7e779c5fd504
Dovid Rosoff <Dovid.rosoff@gmail.com>
2020-11-18 20:38:32 -0500

    Update driveTrain.java
    Updated drive train with basic functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrain.java
@@ -10,6 +10,8 @@ public class driveTrain extends LinearOpMode {
     protected DcMotor fr,fl,br,bl;
     private ElapsedTime time = new ElapsedTime();
 
+    public boolean isBlue = false;
+
     private static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
     private static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
     private static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
@@ -21,20 +23,41 @@ public class driveTrain extends LinearOpMode {
 
     }
 
-    public void setup(){
+    public void setup(HardwareMap ahwmap){
+        HardwareMap hwmap = ahwmap;
+        if(isBlue){
+            fl = hardwareMap.dcMotor.get("Fl");
+            fr = hardwareMap.dcMotor.get("Fr");
+            bl = hardwareMap.dcMotor.get("Bl");
+            br = hardwareMap.dcMotor.get("Br");
+        } else {
+            fr = hardwareMap.dcMotor.get("Fl");
+            fl = hardwareMap.dcMotor.get("Fr");
+            br = hardwareMap.dcMotor.get("Bl");
+            bl = hardwareMap.dcMotor.get("Br");
+        }
 
     }
 
-    public void driveForward(){
-
+    public void driveForward(double power){
+        fr.setPower(power);
+        fl.setPower(power);
+        br.setPower(power);
+        bl.setPower(power);
     }
 
-    public void turnLeft(){
-
+    public void turnLeft(double power){
+        fr.setPower(power);
+        fl.setPower(-power);
+        br.setPower(power);
+        bl.setPower(-power);
     }
 
-    public void turnRight(){
-
+    public void turnRight(double power){
+        fr.setPower(-power);
+        fl.setPower(power);
+        br.setPower(-power);
+        bl.setPower(power);
     }
 }
 
