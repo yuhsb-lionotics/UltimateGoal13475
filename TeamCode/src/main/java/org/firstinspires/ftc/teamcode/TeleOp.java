1f9dbcf6de0e65a0eeeec0e41b3556224930a335
Mangowatz <zmankowitz@yuhsbstudents.org>
2021-04-11 15:29:28 -0400

    Clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -6,45 +6,48 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
-
-
-
-///@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Manual")
-
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Teleop")
-
-public class TeleOp extends LinearOpMode{
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Manual")
+public class TeleOp extends LinearOpMode {
 
     //Written by Emmett. I don't know what your DriveTrain class is supposed to do, so you might
     //want to delete the code I wrote and replace it with something relying on DriveTrain.
 
-    DcMotor fl, bl, fr, br;
+    private DcMotor fl = null;
+    private DcMotor bl = null;
+    private DcMotor br = null;
+    private DcMotor fr = null;
 
     @Override
     public void runOpMode(){
         setup();
         waitForStart();
+        telemetry.addData("Status:", "Ready");
+        telemetry.addData("opModeIsActive: ",opModeIsActive());
+        telemetry.update();
         while(opModeIsActive()){
-        fl.setPower(.7 * -gamepad1.left_stick_y);
-        bl.setPower(.7 * -gamepad1.left_stick_y);
-        fr.setPower(.7 * -gamepad1.right_stick_y);
-        br.setPower(.7 * -gamepad1.right_stick_y);
-        telemetry.addData("Stick x",this.gamepad1.left_stick_x) ;
-        telemetry.addData("Stick y",this.gamepad1.left_stick_y);
-    }}
+            telemetry.addData("stick",gamepad1.left_stick_x);
+            telemetry.addData("stick of other:", gamepad2.left_stick_x);
+            fl.setPower(.7 * -gamepad1.left_stick_y);
+            bl.setPower(.7 * -gamepad1.left_stick_y);
+            fr.setPower(.7 * -gamepad1.right_stick_y);
+            br.setPower(.7 * -gamepad1.right_stick_y);
+            telemetry.update();
+
+    }
 
 
+    }
 
     public void setup() {
-        fl = hardwareMap.dcMotor.get("Fl");
-        bl = hardwareMap.dcMotor.get("Bl");
-        fr = hardwareMap.dcMotor.get("Fr");
-        br = hardwareMap.dcMotor.get("Br");
+        fr = hardwareMap.get(DcMotor.class, "Fr");
+        fl = hardwareMap.get(DcMotor.class, "Fl");
+        br = hardwareMap.get(DcMotor.class, "Br");
+        bl = hardwareMap.get(DcMotor.class, "Bl");
 
-        fl.setDirection(DcMotor.Direction.REVERSE);
-        bl.setDirection(DcMotor.Direction.REVERSE);
         fr.setDirection(DcMotor.Direction.FORWARD);
+        fl.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.FORWARD);
+        bl.setDirection(DcMotor.Direction.REVERSE);
 
     }
 }
