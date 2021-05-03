200b8b4f8341418cddff0623f7c8daba706102af
prosenfeld <prosenfeld@yuhsbstudents.org>
2021-05-03 19:54:57 -0400

    Attempting to fix driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -5,8 +5,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Encoder test")
+
 public class EncoderTest extends DriveTrain {
+
     private final ElapsedTime runtime = new ElapsedTime();
+
     static final double COUNTS_PER_MOTOR_REV = 1680;    // eg: TETRIX Motor Encoder
     static final double DRIVE_GEAR_REDUCTION = 0.5;     // This is < 1.0 if geared UP
     static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
@@ -18,16 +21,25 @@ public class EncoderTest extends DriveTrain {
         setup();
         waitForStart();
         try {
-            int newFRTarget = fr.getCurrentPosition() + (int) (30 * COUNTS_PER_INCH);
-            int newFLTarget = fl.getCurrentPosition() + (int) (30 * COUNTS_PER_INCH);
-            int newBLTarget = bl.getCurrentPosition() + (int) (30 * COUNTS_PER_INCH);
-            int newBRTarget = br.getCurrentPosition() + (int) (30 * COUNTS_PER_INCH);
+            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-            fr.setTargetPosition(newFRTarget);
-            fl.setTargetPosition(newFLTarget);
-            bl.setTargetPosition(newBLTarget);
-            br.setTargetPosition(newBRTarget);
+            int newFRTarget = fr.getCurrentPosition() + (int) (30.0 * COUNTS_PER_INCH);
+            int newFLTarget = fl.getCurrentPosition() + (int) (30.0 * COUNTS_PER_INCH);
+            int newBLTarget = bl.getCurrentPosition() + (int) (30.0 * COUNTS_PER_INCH);
+            int newBRTarget = br.getCurrentPosition() + (int) (30.0 * COUNTS_PER_INCH);
+
+            fr.setTargetPosition(2000);
+            fl.setTargetPosition(2000);
+            bl.setTargetPosition(2000);
+            br.setTargetPosition(2000);
             runtime.reset();
+            fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             fr.setPower(0.4);
             fl.setPower(0.4);
             bl.setPower(0.4);
@@ -50,7 +62,7 @@ public class EncoderTest extends DriveTrain {
                     "Encoder Drive",
                     "Finished in %.2f s/%f",
                     runtime.seconds(),
-                    10
+                    10.0
             );
             telemetry.update();
 
@@ -59,12 +71,6 @@ public class EncoderTest extends DriveTrain {
             fl.setPower(0);
             bl.setPower(0);
             br.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
         catch (Exception exception){
             telemetry.addData("exception",exception.getStackTrace());
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimedAuto.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="TimedAuto", group="LinearOpMode")
+
+public class TimedAuto extends DriveTrain {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 1680;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 0.5;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    @Override
+    public void runOpMode() {
+        setup();
+        waitForStart();
+        runtime.reset();
+
+        // for moving wobble goal:
+        driveForward(0.4);
+        sleep(5400);
+        driveForward(-0.4);
+        sleep(2000);
+        driveForward(0);
+    }
+
+}
